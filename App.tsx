1f6c3be97bbd7a3dbd2806cc9616f332102912f5
Khubaib <khubaib@stoqo.com>
2020-05-12 17:44:49 +0500

    Setup base project with typescript

---
+++ App.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+declare const global: {HermesInternal: null | {}};
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}>
+          <View>
+            <Text>Hello World</Text>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: 'gray',
+  },
+});
+
+export default App;
